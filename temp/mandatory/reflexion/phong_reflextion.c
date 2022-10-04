69f3f29c7ff7b191c15cd5d705650299fff07fc1
Gyeongdong Yu <gyyu@c8r7s3.42seoul.kr>
2022-10-04 20:27:20 +0900

    8:27

--- temp/mandatory/reflexion/phong_reflextion.c
+++ temp/mandatory/reflexion/phong_reflextion.c
@@ -6,7 +6,7 @@
 /*   By: gyyu <gyyu@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:54:24 by iyun              #+#    #+#             */
-/*   Updated: 2022/10/04 16:39:33 by gyyu             ###   ########.fr       */
+/*   Updated: 2022/10/04 19:43:48 by gyyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_point	bmp(t_point	nor_vec, t_meet meet_point, t_minirt info)
 	t_point	tangent;
 	t_point	bitangent;
 
-	if (vec3_to_uv(meet_point, &u, &v, info) == 0)
+	if (vec3_to_uv(meet_point, &u, &v, info) == 0.00000000)
 	{
 		u *= (double)info.bmp.width;
 		v *= (double)info.bmp.height;
@@ -32,14 +32,18 @@ t_point	bmp(t_point	nor_vec, t_meet meet_point, t_minirt info)
 	// else
 	// 	new_nor_vec = nor_vec;
 	// printf("x = %d, y = %d, z = %d\n", bmp.red, bmp.green, bmp.blue);
-	temp.x = (double)(bmp.red * 2 - 1);
-	temp.y = (double)(bmp.green * 2 - 1);
-	temp.z = (double)(bmp.blue * 2 - 1);
+	temp.x = (double)(bmp.red * 2);
+	temp.y = (double)(bmp.green * 2);
+	temp.z = (double)(bmp.blue * 2);
+	// set_unit_vec(&temp);
 	cross_product(nor_vec, temp, &tangent);
+	// set_unit_vec(&tangent);
 	cross_product(tangent, nor_vec, &bitangent);
+	// set_unit_vec(&bitangent);
 	new_nor_vec.x = (tangent.x * meet_point.meet.x + bitangent.x * meet_point.meet.y + nor_vec.x * meet_point.meet.z);
 	new_nor_vec.y = (tangent.y * meet_point.meet.x + bitangent.y * meet_point.meet.y + nor_vec.y * meet_point.meet.z);
 	new_nor_vec.z = (tangent.z * meet_point.meet.x + bitangent.z * meet_point.meet.y + nor_vec.z * meet_point.meet.z);
+	printf("x = %lf, y = %lf, z = %lf\n", new_nor_vec.x, new_nor_vec.y, new_nor_vec.z);
 	return (new_nor_vec);
 }
 
@@ -51,6 +55,8 @@ void	phong_reflexion(t_meet meet_point, t_light light, t_minirt info, t_phong *d
 
 	normal_vec = ft_normal_vec(meet_point, info);
 	*normal_vec = bmp(*normal_vec, meet_point, info);
+	// printf("x = %lf, y = %lf, z = %lf\n", normal_vec->x, normal_vec->y, normal_vec->z);
+	
 	set_unit_vec(normal_vec);
 	correlation = ft_correlation(meet_point, light, info, *normal_vec);
 	if (correlation == OUT_OUT || correlation == IN_IN)
